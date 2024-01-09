dcd28aaf17d1704198d081c1e8ba75302d61a661
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-09 11:55:35 +0300

    Some changes

--- EmployeeApi/Program.cs
+++ EmployeeApi/Program.cs
@@ -1,10 +1,19 @@
+using EmployeeApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
+builder.Services.AddEndpointsApiExplorer(); // Her API bir son noktasý vardýr.
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
