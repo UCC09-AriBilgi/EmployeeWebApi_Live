a58fa62057e82c590c5253c4cafe3f95c17a1081
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-09 14:38:38 +0300

    Some changes

---
+++ EmployeeWeb/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeWeb.Models;
+using System.Collections.Generic;
+
+namespace EmployeeWeb.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        // Db üzerinden tüm kayıtları getirecek olan metot/task
+        Task<IEnumerable<Employee>> GetAll();
+
+        // Db üzerinden istenen bir kayıdı olan metot/task
+        Task<Employee> GetById(int id);
+
+    }
+}
