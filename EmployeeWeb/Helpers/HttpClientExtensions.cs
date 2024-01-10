328f368645663b92b11e5919efabc18d29b81dcf
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-10 10:50:17 +0300

    Some changes

---
+++ EmployeeWeb/Helpers/HttpClientExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace EmployeeWeb.Helpers
+{
+    public static class HttpClientExtensions
+    {
+        // HttpClient işlemleri sonucu gelecek olan bilgiyi çözümleyecek.
+        // Çünkü bilgi json formatında olacağı için
+
+        public static async Task<T> ReadContentAsync<T>(this HttpResponseMessage response)
+        {
+            // Bana API tarafından gönderilen response(cevap) uygun durumda mı
+            if (response.IsSuccessStatusCode == false)
+            {
+                throw new ApplicationException($"API çağrılırken problem : {response.Content}"); // ?? response.ReasonPhrase
+            }
+
+            // eğer durum uygunsa
+            // API tarafından gönderilen/gelen veri okunacak...
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            //Bu veri normal text formatında..Bunu Json haline çevirmece
+            var result = JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return result;
+
+        }
+    }
+}
---
+++ EmployeeWeb/Services/EmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeWeb.Helpers;
+using EmployeeWeb.Models;
+using EmployeeWeb.Services.Interfaces;
+
+namespace EmployeeWeb.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        // Bu MVC uygulaması client/server mimarisi yapısında client görevi görecek. O yüzden bazı gerekli kütüphaneleri tanımlamak gerekiyor.
+
+        private readonly HttpClient _client; // müşteri
+        private readonly string _Apibase; // https://localhost:7087/
+
+        public EmployeeService(HttpClient client,IConfiguration configuration)
+        {
+            _client = client;
+            _Apibase = configuration["APISection:BaseAddress"]; // appsettings.json dan gelen bilgiler
+        }
+
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            // burada bir weblinki oluşturuyoruz.
+            string ApiPath = _Apibase + "api/EmployeeEF";
+
+            var response = await _client.GetAsync(ApiPath);
+
+            return await response.ReadContentAsync<List<Employee>>(); // karsı taraftan gelen bilgiyi okuyacak metot.******
+        }
+
+        public Task<Employee> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
