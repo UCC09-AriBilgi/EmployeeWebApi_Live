bafadf3e6a7305819f02f594c55aac6fa0efd4ee
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-10 11:53:31 +0300

    Some changes

--- EmployeeWeb/Controllers/EmployeeController.cs
+++ EmployeeWeb/Controllers/EmployeeController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeWeb.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace EmployeeWeb.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        // Burası bizim sayfalarımı kontrol edecek controller
+
+        private readonly IEmployeeService _service;
+        private readonly string _ApiBase;
+        private readonly IWebHostEnvironment _environmet; // 2 uygulama arası geliş/gidiş old için
+
+        public EmployeeController(IEmployeeService service , IConfiguration configuration, IWebHostEnvironment environment)
         {
-            return View();
+            _service = service;
+            _environmet = environment;
+            _ApiBase = configuration["APISection:BaseAddress"];
+                
+        }
+
+
+        public async Task<IActionResult> Index(int page=1) //
+        {
+            var employees=await _service.GetAll(); // Servisim üzerinden API tarafına bağlanıyorum veriyi alacağım.
+
+            return View(await employees.ToPagedListAsync(page,10)); // Çok kayıtlı veri dönüşünü sayfalama şeklinde gösterebilmek için bir Nuget paketi yüklenmesi gerekiyor (X.PagedList.Mvc.Core)
         }
     }
 }
